b0cdd966ced341e8e3964b12ae9cbc20b25885b9
Mercyma-AI <mercyma323@gmail.com>
2024-10-17 18:58:27 +0300

    Add files via upload

---
+++ Prog A.c
@@ -0,0 +1,23 @@
+//program to calculate customer loan
+#include<stdio.h>
+#include<math.h>
+int main(){
+	int a;
+	float i;
+	printf("age:",a);
+	scanf("%d",a);
+	printf("income",i);
+	scanf("%d",i);
+	if
+	(a >=21 && i >= 21000){
+		printf("Congratulations u have qualified for the loan");
+		}
+		else{
+   printf("Unfortunately we are unable to offer you  loan at this time");
+}
+
+    return 0;
+   	}
+	
+	
+    
